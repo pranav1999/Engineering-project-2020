02b1f913cbdbca6e1b10ed923e0ffa74ffc929ac
Lakshya3119 <63750702+Lakshya3119@users.noreply.github.com>
2021-05-28 10:29:14 +0530

    Update SubBiology.js
    Bio 9-10 updated

--- caltpk/src/SubBiology.js
+++ caltpk/src/SubBiology.js
@@ -1,6 +1,5 @@
 //Biology experimets data
 
-
 export const class9 = [
     {
         id:1,
@@ -15,52 +14,54 @@ export const class9 = [
         id:2,
         title:"Slide of Onion peel and cheek cells",
         img:"https://images.pexels.com/photos/144206/pexels-photo-144206.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-        description:"ndustry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in",
-        videolink:"https://www.youtube.com/watch?v=GxyfS1v77lU",
+        description:"With the help of the toothpick scrap the inner side of the cheek to get some epithelial cells. Place the scrappings in a drop of water over a clean slide. ... Add a drop of glycerine on the slide. With the help of needle, gently place the coverslip over the slide to avoid entry of air bubbles",
+        videolink:"https://youtu.be/UyP3-ICurVk",
         explink:"",
-        theory:""
+        theory:"https://www.cbsesamplepapers.info/cbse/cbse-class-9-science-practical-skills-slide-of-onion-peel-and-cheek-cells"
     },
     {
         id:3,
         title:"Plants and animal tissue",
         img:"https://images.pexels.com/photos/2062424/pexels-photo-2062424.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
-        description:"ndustry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in",
-        videolink:"https://www.youtube.com/watch?v=GxyfS1v77lU",
+        description:"Plant tissues include xylem, phloem, collenchyma, sclerenchyma, epidermis and meristematic tissue. Animal tissues include epithelial tissue, connective tissue, muscle tissue and nerve tissue. ... The leaf is an example of a plant organ and its structure is adapted to its role in photosynthesis.",
+        videolink:"https://youtu.be/qTtKi4rvbI4",
         explink:"",
-        theory:""
+        theory:"https://www.cbsesamplepapers.info/cbse/cbse-class-9-science-practical-skills-plant-and-animal-tissues"
     }
 
 ]
+
 
 export const class10 = [
     {
         id:1,
-        title:"Experiment 9",
-        img:"https://images.pexels.com/photos/62600/pexels-photo-62600.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
-        description:"ndustry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in",
-        videolink:"https://www.youtube.com/watch?v=GxyfS1v77lU",
-        explink:"https://simmer.io/@Buoyancy/buoyancy",
-        theory:""
+        title:"Life cycle of mosquito",
+        img:"https://www.pexels.com/photo/black-mosquito-on-person-s-skin-169357/",
+        description:"All mosquito species go through four distinct stages during their life cycle: Egg - hatches when exposed to water. Larva - (plural: larvae) wriggler lives in water; molts several times; most species surface to breathe air. Pupa - (plural: pupae) tumblerdoes not feed; stage just before emerging as adult",
+        videolink:"https://youtu.be/mroLfTPhL90",
+        explink:"",
+        theory:"https://lakshya3119.github.io/Bio/All-Bio-Class9/exp-1.html"
     },
     {
         id:2,
-        title:"Experiment 9",
-        img:"https://images.pexels.com/photos/62600/pexels-photo-62600.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
-        description:"ndustry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in",
-        videolink:"https://www.youtube.com/watch?v=GxyfS1v77lU",
+        title:"External features of roots",
+        img:"https://www.pexels.com/photo/photo-of-potted-plants-on-wooden-table-4503273/",
+        description:"The study reveals that dicot plants have tap root system, broad leaves with reticulate venation, five or in multiple of five sepals, petals and stamens in their flowers whereas monocots have fibrous root, narrow leaves with parallel venation, three or in multiple of three sepals, petals and stamens in their flowers",
+        videolink:"https://youtu.be/8IjN1ZAkIn8",
         explink:"",
-        theory:""
+        theory:"https://lakshya3119.github.io/Bio/All-Bio-Class9/exp-2.html"
     },
     {
         id:3,
-        title:"Experiment 9",
-        img:"https://images.pexels.com/photos/62600/pexels-photo-62600.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
-        description:"ndustry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in",
-        videolink:"https://www.youtube.com/watch?v=GxyfS1v77lU",
+        title:"photosynthesis",
+        img:"https://www.pexels.com/photo/summer-leaf-tree-dew-5403480/",
+        description:"Photosynthesis is a process used by plants and other organisms to convert light energy into chemical energy that, through cellular respiration, can later be released to fuel the organism's metabolic activities.",
+        videolink:"https://youtu.be/3RBl3xqnCrc",
         explink:"",
-        theory:""
+        theory:"https://www.cbsetuts.com/ncert-class-10-science-lab-manual-light-necessary-photosynthesis/"
     }
 ]
+
 
 export const class11 = [
     {
